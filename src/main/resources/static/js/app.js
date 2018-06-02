526b12e9bf9610dbdf142eb800b9f31ce06a731e
Marcio Alexandre <marcio.alexandre83@gmail.com>
2018-06-02 11:54:15 -0300

    add url to js controller

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -15,7 +15,7 @@ app.controller('xbrlController', ['$scope', 'Upload', '$timeout', function ($sco
 	        	if (file.size <= 15000000) {
 	        		
 	 	            file.upload = Upload.upload({
-		                url: 'https://xbrl-json.herokuapp.com/upload',
+		                url: 'https://xbrlframework.herokuapp.com/upload',
 	 	            	//url: 'http://localhost:8080/upload',
 		                data: {apifile: file}
 		            });
